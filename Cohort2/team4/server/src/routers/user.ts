27bd01f3a54914d789f430f45be34ddf967cc951
Michelle Song <michssong@gmail.com>
2023-07-22 22:19:58 -0500

    Removed user_id from routers/user.ts. Adjusted variables for calls due to lint issues.

--- Cohort2/team4/server/src/routers/user.ts
+++ Cohort2/team4/server/src/routers/user.ts
@@ -6,7 +6,6 @@ import { randomUUID } from 'crypto';
 
 
 type User = {
-  user_id: string
   email: string
   first_name: string
   last_name: string
@@ -14,14 +13,11 @@ type User = {
 };
 
 const USERS: User[] = [
-  { user_id: '1', email: 'kyle@test.com', first_name: 'Kyle', last_name: 'Flob', auth_key:'test1' },
-  { user_id: '2', email: 'julie@test.com', first_name: 'Julie', last_name: 'Bob', auth_key: 'test2' },
+  { email: 'kyle@test.com', first_name: 'Kyle', last_name: 'Flob', auth_key:'test1' },
+  { email: 'julie@test.com', first_name: 'Julie', last_name: 'Bob', auth_key: 'test2' },
 ];
 
 export const userRouter = router({
-  byId: publicProcedure.input(z.string()).query(req => {
-    return USERS.find(user => user.user_id === req.input);
-  }),
   byEmail: publicProcedure.input(z.string()).query(req => {
     return USERS.find(user => user.email === req.input);
   }),
@@ -29,10 +25,10 @@ export const userRouter = router({
     return USERS.find(user => user.auth_key === req.input);
   }),
   create: publicProcedure
-    .input(z.object({ email: z.string(), first_name: z.string(), last_name:z.string(), auth_key:z.string() }))
+    .input(z.object({ email: z.string(), firstName: z.string(), lastName:z.string(), auth_key:z.string() }))
     .mutation(req => {
-      const { email, first_name, last_name } = req.input;
-      const user: User = { user_id: randomUUID(), email, first_name, last_name, auth_key:'testkey' };
+      const { email, firstName, lastName } = req.input;
+      const user: User = { email, first_name: firstName, last_name: lastName, auth_key:'test_key' };
       USERS.push(user);
       return user;
     }),
