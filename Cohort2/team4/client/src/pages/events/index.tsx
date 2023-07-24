a01d6b585a51e7117231ee06abc1b826f4380256
Jonathan Romain <romainjonathan43@gmail.com>
2023-07-24 18:56:01 -0500

    Added team name, sign in, and add new event button

--- Cohort2/team4/client/src/pages/events/index.tsx
+++ Cohort2/team4/client/src/pages/events/index.tsx
@@ -1,13 +1,28 @@
-import EventList from '../../components/EventList';
+import EventList from "../../components/EventList";
 
 export default function EventDashboard() {
-	const events = ['event1'];
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>
-				<div>TODO -- Replace with event dashboard</div>
-				<EventList events={events}/>
-			</div>
-		</main>
-	);
+  const user = "Sally";
+  const events = ["event1"];
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <header className="flex items-center gap-[70vw]">
+          Convi
+          <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
+            Sign in
+          </button>
+        </header>
+      </div>
+      <div>
+        <div className="space-y-10">
+          <h1 className="font-bold text-2xl">Hello there, {user}</h1>
+          <p>See your upcoming events</p>
+          <EventList events={events} />
+          <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec] m-10">
+            Add New Event
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
