0fca6878d68584ed7a7fcb53fa0954b0b0c19fab
Jonathan Romain <romainjonathan43@gmail.com>
2023-07-28 19:22:30 -0500

    Added Style to event dashboard

--- Cohort2/team4/client/src/pages/events/index.tsx
+++ Cohort2/team4/client/src/pages/events/index.tsx
@@ -4,23 +4,51 @@ export default function EventDashboard() {
   const user = "Sally";
   const events = ["event1"];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <header className="flex items-center gap-[70vw]">
-          Convi
-          <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
-            Sign in
-          </button>
-        </header>
-      </div>
-      <div>
-        <div className="space-y-10">
+    <main style={{ height: "100dvh" }}>
+      <div className="grid grid-row-1 gap-12 my-20 mx-auto w-3/5 h-2/3 ">
+        <div className="bg-[#f1f5f9] space-y-10 border-grey border-solid border-2 pl-20 pt-20 rounded-md">
           <h1 className="font-bold text-2xl">Hello there, {user}</h1>
+          <div className="grid grid-cols-2">
           <p>See your upcoming events</p>
-          <EventList events={events} />
-          <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec] m-10">
-            Add New Event
-          </button>
+          <p className="">Days</p>
+          </div>
+          <div className="grid grid-cols-2 justify-between">
+            <form action="" className="flex flex-col space-y-4">
+            <a href="#"className="bg-[#f8fafc] w-96 border rounded-md"><EventList events={events} /></a>
+              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
+                event2
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
+                event3
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
+                event4
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
+                event5
+              </a>
+            </form>
+            <form action="" className="flex flex-col space-y-4">
+              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
+                3 Days Remaining
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
+                10 Days Remaining
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
+                15 Days Remaining
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
+                17 Days Remaining
+              </a>
+              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
+                33 Days Remaining
+              </a>
+            </form>
+          </div>
+            <button className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-400 mt-10">
+              Add New Event
+            </button>
         </div>
       </div>
     </main>
