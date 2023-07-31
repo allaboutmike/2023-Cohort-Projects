48bf54bf921b01f6ca81e9b8b3239d413ac0b811
Trey Clark <itreyclark@gmail.com>
2023-07-30 22:18:32 -0500

    fixes linting issues and adds some Link components

--- Cohort2/team4/client/src/components/NavBar.tsx
+++ Cohort2/team4/client/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 
 import logo from "../images/ConviLogo.png"
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from "next/link";
 
 
 
@@ -14,10 +15,10 @@ const NavBar = () => {
         <div className="mb-6">
             <nav className="flex items-center justify-between flex-wrap">
                 <div className="flex items-center flex-shrink-0 text-white mr-8">
-                    <a href="/" className="flex flex-row items-center">
+                    <Link href="/" className="flex flex-row items-center">
                     <img src={logo.src} alt="ConviLogo" className="transform scale-150 h-16 "/>
                     <span className="text-xl text-black">Convi</span>
-                    </a>
+                    </Link>
                 </div>
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div className="text-sm lg:flex-grow">
@@ -32,16 +33,19 @@ const NavBar = () => {
                         </a>
                         </div>
                     <div className="pt-6">
-                        {user ?
-                        <><a href="/events" className="inline-block text-md px-4 py-2 mr-4 leading-none border rounded text-white bg-blue-500 border-white hover:border-transparent hover:text-white-800 hover:bg-blue-400 mt-4 lg:mt-0">Dashboard</a>
+                        
+                        {/* eslint-disable */
+                        user ?
+                        <>
+                        <Link href="/events" className="inline-block text-md px-4 py-2 mr-4 leading-none border rounded text-white bg-blue-500 border-white hover:border-transparent hover:text-white-800 hover:bg-blue-400 mt-4 lg:mt-0">Dashboard</Link>
                         <a href="/api/auth/logout" className="inline-block text-md px-4 py-2 mr-4 leading-none border rounded text-white bg-red-500 border-white hover:border-transparent hover:text-white-800 hover:bg-red-400 mt-4 lg:mt-0">Logout</a>
                         </>
                         :       
                         <>                 
                         <a href="/api/auth/login" className="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white bg-green-500 hover:border-transparent hover:bg-green-400 mt-4 lg:mt-0">Login</a>
-                        <a href="/api/auth/signup" className="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white bg-blue-500 hover:bg-blue-400 hover: mt-4 lg:mt-0 m-4">Sign Up</a>
+                        <a href="/api/auth/login" className="inline-block text-md px-4 py-2 leading-none border rounded text-white border-white bg-blue-500 hover:bg-blue-400 hover: mt-4 lg:mt-0 m-4">Sign Up</a>
                         </>
-                        }
+                        /* eslint-enable */}
                     </div>
                 </div>
             </nav>
