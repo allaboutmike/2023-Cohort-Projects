d304717dbc88d6459c761e05b1ec0bf695579f0d
Trey Clark <itreyclark@gmail.com>
2023-08-01 16:08:24 -0500

    adds event routes

--- Cohort2/team4/server/src/routers/event.ts
+++ Cohort2/team4/server/src/routers/event.ts
@@ -5,38 +5,42 @@ import { z } from 'zod';
 import { randomUUID } from 'crypto';
 
 
-type Event = {
-  name: String,
-  short_description: String,
-  long_description: String,
-  city: String,
-  state: String,
-  country: String,
-  address: String,
-  start_time: Date,
-  end_time: Date,
-  host: number,
-};
 
-
-const EVENTS: Event[] = []
 
 export const eventRouter = router({
+  getAll: publicProcedure.query((req) => {
+    return Events.findAll();
+  }),
+  byId: publicProcedure.input(z.string()).query(req =>{
+    return Events.findByPk(req.input);
+  }),
   byName: publicProcedure.input(z.string()).query(req => {
-    return EVENTS.find(event => event.name === req.input);
+    return Events.findOne({
+      where: {
+        name: req.input
+      }
+    });
   }),
   byState: publicProcedure.input(z.string()).query(req => {
-    return EVENTS.find(event => event.state === req.input);
+    return Events.findOne({
+      where: {
+        state: req.input
+      }
+    });
   }),
   byCity: publicProcedure.input(z.string()).query(req => {
-    return EVENTS.find(events => events.city === req.input )
+    return Events.findOne({
+      where: {
+        city: req.input
+      }
+    });
   }),
   create: publicProcedure
     .input(z.object({ name: z.string(), short_description: z.string(), long_description: z.string(), city: z.string(), state: z.string(), country: z.string(), address: z.string(), start_time: z.date(), end_time: z.date(), host: z.number() }))
     .mutation(req => {
       const { name, short_description, long_description, city, state, country, address, start_time, end_time, host } = req.input;
-      const event: Event = { name, short_description, long_description, city, state, country, address, start_time, end_time, host }
-      EVENTS.push(event);
-      return event;
+      const newEvent = { name, short_description, long_description, city, state, country, address, start_time, end_time, host }
+      Events.create(newEvent);
+      return newEvent;
     }),
 });
