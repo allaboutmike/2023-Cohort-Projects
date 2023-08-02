707415d91ecd1b13fc648e73986a51321258b8f7
Jonathan Romain <romainjonathan43@gmail.com>
2023-08-02 09:59:51 -0500

    Removed the events list from the events index.tsx file and added it to the components eventList.tsx file

--- Cohort2/team4/client/src/components/EventList.tsx
+++ Cohort2/team4/client/src/components/EventList.tsx
@@ -1,7 +1,88 @@
+const events = [
+  {
+    event_id: 1,
+    name: "Tech Conference 2023",
+    short_description: "Cutting-edge tech talks",
+    long_description:
+      "Join us for a tech conference featuring talks on AI, blockchain, and more.",
+    city: "New York City",
+    state: "New York",
+    country: "United States",
+    address: "123 Tech Street",
+    start_time: "2023-07-20T09:00:00.000Z",
+    end_time: "2023-07-21T18:00:00.000Z",
+    host: 1,
+  },
+  {
+    event_id: 2,
+    name: "Music Festival 2023",
+    short_description: "Live performances and fun",
+    long_description:
+      "A weekend of live music with top bands and exciting activities.",
+    city: "Los Angeles",
+    state: "California",
+    country: "United States",
+    address: "456 Music Avenue",
+    start_time: "2023-07-28T16:00:00.000Z",
+    end_time: "2023-07-30T23:59:00.000Z",
+    host: 2,
+  },
+  {
+    event_id: 3,
+    name: "Art Exhibition Expressions",
+    short_description: "Artworks by local artists",
+    long_description:
+      "Explore various art pieces that express emotions and creativity.",
+    city: "London",
+    state: null,
+    country: "United Kingdom",
+    address: "789 Art Gallery Road",
+    start_time: "2023-08-05T10:00:00.000Z",
+    end_time: "2023-08-10T19:00:00.000Z",
+    host: 3,
+  },
+  {
+    event_id: 4,
+    name: "Food Festival 2023",
+    short_description: "Cuisine from around the world",
+    long_description:
+      "Taste the finest dishes from diverse cultures in this food extravaganza.",
+    city: "Sydney",
+    state: "New South Wales",
+    country: "Australia",
+    address: "10 Foodie Lane",
+    start_time: "2023-08-15T12:00:00.000Z",
+    end_time: "2023-08-17T22:00:00.000Z",
+    host: 1,
+  },
+  {
+    event_id: 5,
+    name: "Gathering 2023",
+    short_description: "This is a gathering of people.",
+    long_description:
+      "This is an even longer description talking about the gathering of people. When you gather a whole bunch of people together, good things should come from it, such as singing, playing, and new ideas. However, it is usually negative things, such as all of the ice cream being eaten. This gathering will make sure there are a ton of ice cream.",
+    city: "Dallas",
+    state: "Texas",
+    country: "United States",
+    address: null,
+    start_time: "2023-10-01T05:01:30.000Z",
+    end_time: "2023-10-02T05:01:30.000Z",
+    host: 1,
+  },
+];
+
+
 export default function EventList(props: { events: Array<string> }) {
 	return (
-		<div>
-			<ul><li>{props.events[0]}</li></ul>
+		<div className="grid ">
+		  <ul>
+			{events.map((event, index) => (
+			  <li key={index} className="p-2 border rounded-md">
+				<div className=" ">{event.name}</div>
+			  </li>
+			))}
+		  </ul>
 		</div>
-	);
-}
+	  );
+  }
+  
--- Cohort2/team4/client/src/pages/events/index.tsx
+++ Cohort2/team4/client/src/pages/events/index.tsx
@@ -10,41 +10,9 @@ export default function EventDashboard() {
           <h1 className="font-bold text-2xl">Hello there, {user}</h1>
           <div className="grid grid-cols-2">
           <p>See your upcoming events</p>
-          <p className="">Days</p>
           </div>
-          <div className="grid grid-cols-2 justify-between">
-            <form action="" className="flex flex-col space-y-4">
-            <a href="#"className="bg-[#f8fafc] w-96 border rounded-md"><EventList events={events} /></a>
-              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
-                event2
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
-                event3
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
-                event4
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-96 border rounded-md">
-                event5
-              </a>
-            </form>
-            <form action="" className="flex flex-col space-y-4">
-              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
-                3 Days Remaining
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
-                10 Days Remaining
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
-                15 Days Remaining
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
-                17 Days Remaining
-              </a>
-              <a href="#" className="bg-[#f8fafc] w-80 border rounded-md">
-                33 Days Remaining
-              </a>
-            </form>
+          <div className="flex">
+            <a href="#"className="bg-[#f8fafc]"><EventList events={events} /></a>
           </div>
             <button className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-400 mt-10">
               Add New Event
