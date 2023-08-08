cded996cb589eac83dc94c9b59f185fc6754c481
Michelle Song <michssong@gmail.com>
2023-08-08 14:11:10 -0500

    Removed extra tag in [id].tsx

--- Cohort2/team4/client/src/pages/events/[id].tsx
+++ Cohort2/team4/client/src/pages/events/[id].tsx
@@ -2,10 +2,8 @@ import EventCard from "@/components/EventCard";
 
 export default function EventPage() {
     return (
-        <>
-            <div className='p-6'>
-                <EventCard/>
-            </div>
-        </>
+        <div className='p-6'>
+            <EventCard/>
+        </div>
     )
 }
