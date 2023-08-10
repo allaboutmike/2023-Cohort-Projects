953d630c29683e613ac1663a45de7b072818eedf
Mike B <bowman.michael@gmail.com>
2023-08-09 23:37:41 -0400

    CHORE: Extra slash in API URL

--- Cohort2/team4/client/src/utils/trpc.ts
+++ Cohort2/team4/client/src/utils/trpc.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 function getBaseUrl() {
   if(process.env.NODE_ENV === 'production'){
-    return 'https://team4-eventmanagementapi-production.up.railway.app/'
+    return 'https://team4-eventmanagementapi-production.up.railway.app'
   }
   return `http://localhost:5000`;
 }
